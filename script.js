7649aed88800267d8666e2a2934e2de6846e7914
Anita Dan <anita@Anitas-MacBook-Air.local>
2020-10-24 13:55:18 -0400

    Seven day API

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ $(document).ready(function () {
 
     var APIkey = "11aae01829609ac12c0335ac0cc4505c";
 
-
-
     $("button").on("click", function () {
-
 
         var userInput = $("#search-box").val();
 
-        var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=" + APIkey
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIkey
+        // "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
 
 
         $.ajax({
@@ -18,34 +17,67 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
 
-
-            $("#seven-days").empty();
-
-            var weathResults = response.list;
+            var weathResults = response;
             console.log(weathResults);
 
-            // for (var i = 0; i < 7; i++) {
-                var dayoneDiv = $("<div>")
-                
-                var pTemp = $("<p>").text("Temp: " + weathResults[0].main.temp);
-                var pWeath = $("<p>").text("Weather: " + weathResults[0].weather[0].icon)
-                
-                dayoneDiv.append(pTemp, pWeath)
-                $("#seven-days").append(dayoneDiv)
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            var temp = response.main.temp;
+            var weather = response.weather[0].icon;
 
-            // var dayoneDiv = $("<div>")
-            // var pTemp = $("<p>").text("Temp: " + results[0].main.temp);
-            // var pWeath = $("<p>").text("Weather: " + results[i].weather[0].icon)
-            // dayoneDiv.append(date, pTemp, pWeath, pHumid)
+            console.log(lat, lon);
+
+            var curDiv = $("<div>");
+            var pTemp = $("<p>").text("Temperature: " + temp);
+            var pWeath = $("<p>").text("Weather: " + weather);
+            curDiv.append(pTemp, pWeath)
+
+            $("#seven-days").append(curDiv)
+
+            sevenDay(lat, lon)
+
+        })
+    });
+    function sevenDay(lat, lon) {
+
+        var userInput = $("#search-box").val();
+
+        var sevDURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={current}" + "&appid=" + APIkey + "&units=imperial"
+
+        console.log(lat, lon)
 
 
+        $.ajax({
+            url: sevDURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.daily)
+            // var sevResults = response.list;
+            // console.log(sevResults);
 
 
+            var daily = response.daily
+            
 
-            // }
+                for (var i = 0; i < daily.length; i++) {
+                    var unixTime = daily[i].dt
+                    var date = moment.unix(unixTime).format("MM, DD, YYYY")
+                    console.log(date);
+
+
+                    
+
+                }
+            
+
+
         })
 
-    });
+
+    };
+
+
 
 
 
